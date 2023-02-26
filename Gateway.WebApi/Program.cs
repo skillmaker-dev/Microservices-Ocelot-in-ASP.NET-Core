15ab45e2864be1ac8c326381b62252deb10ffd54
Anas Chahid <anas.chahid777@gmail.com>
2023-02-27 00:35:09 +0100

    Add Ocelot gateway

--- Gateway.WebApi/Program.cs
+++ Gateway.WebApi/Program.cs
@@ -1,6 +1,24 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
+// Add ocelot configuration file and injecting ocelot
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+builder.Services.AddOcelot();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (builder.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+app.UseRouting();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
+
+await app.UseOcelot();
 
 app.Run();
